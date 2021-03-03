3f5917858af5cff77809f3269c03d08f3931f93e
JavierMJK <javier.garcia@xyleminc.com>
2021-03-03 12:11:00 +0100

    create serial line pdu

--- sanbox/master.py
+++ sanbox/master.py
@@ -2,13 +2,14 @@ import serial
 import time
 
 # from modbus_lib.modbus_serial import receive_for_master
-from modbus_lib.modbus_serial import send_request_unicast
+from modbus_lib.modbus_serial import ModbusSerialLayer
 import modbus_lib.exceptions as execps
 
 
 
 def run():
     ser = serial.Serial('COM39', 115200)
+    master = ModbusSerialLayer(ser, 1, 0.01)
     print('\nmaster running')
     try:
         # print()
@@ -23,7 +24,7 @@ def run():
             print()
             # time.sleep(1)
             try:  
-                rx_buffer = send_request_unicast(ser, 1, bytearray([3,0,126,0,2]))
+                rx_buffer = master.send_request_unicast(1, bytearray([3,0,126,0,2]))
                 print('RX: {:25}, total:{}, timeouts:{}, frameNOKs:{}'.format( rx_buffer.hex(), total, timeout_errors, frameNOK_errors) )
             except execps.ResponseTimeoutError:
                 # print("ResponseTimeoutError exception catched!!!")
--- sanbox/slave.py
+++ sanbox/slave.py
@@ -2,41 +2,40 @@ import serial
 import time
 from threading import Thread, Event
 
-from modbus_lib.modbus_serial import receive_for_slave
+from modbus_lib.modbus_serial import ModbusSerialLayer
 
 
 var = 0
 event = Event()
 
-def run_slave(ser):
+def run_slave(slave):
     global var
 
     while True:
         print()
-        rx_buffer = receive_for_slave(ser)
+        rx_buffer = slave.receive_for_slave()
         print('RX:', rx_buffer.hex())
         tx_buffer = bytearray([1,3,4,204,205,62,76,69,9])
-        ser.write(tx_buffer)
-        print('TX:', tx_buffer.hex())
+        slave.send_frame(tx_buffer)
+        # print('TX:', tx_buffer.hex())
     
-def modify_var():
-    global var
-    while True:
-        var = var + 1
-        time.sleep(1)
-        if event.is_set():
-            break
-
-
+# def modify_var():
+#     global var
+#     while True:
+#         var = var + 1
+#         time.sleep(1)
+#         if event.is_set():
+#             break
 
 def run():
     ser = serial.Serial('COM38', 115200)
+    slave = ModbusSerialLayer(ser, 1, 0.01)
     print('\nslave running')
-    t_modify_var = Thread(target=modify_var)
-    t_modify_var.start()
+    # t_modify_var = Thread(target=modify_var)
+    # t_modify_var.start()
     try:
-        run_slave(ser)
+        run_slave(slave)
     finally:
-        event.set()
-        t_modify_var.join()
+        # event.set()
+        # t_modify_var.join()
         ser.close() 
