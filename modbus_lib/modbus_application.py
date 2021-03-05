9db8e7298ee03018ccdd856432da787102b76c24
JavierMJK <javier.garcia@xyleminc.com>
2021-03-05 12:02:15 +0100

    added logger to ModbusApplicationLayer and exceptions

--- modbus_lib/modbus_application.py
+++ modbus_lib/modbus_application.py
@@ -1,51 +1,60 @@
 import struct
+import logging
 
 import modbus_lib.exceptions as exceps
 
-# class Response():
-#     def __init__(self, requested_function, response_pdu):
-#         self.function = response_pdu[0]
-#         self.data = None
-#         self.exception_code = None
-#         if self.function == requested_function:
-#             self.data = response_pdu[1:]
-#         elif self.function == ( requested_function & 0x80):
-#             self.exception_code = response_pdu[1]
-#             raise exceps.MosbusException
-#         else:
-#             raise exceps.ModbusUnknownException
-            
-        
+    
+class ModbusApplicationLayer():
+    def __init__(self, is_logger_on=True):
+        self.logger = logging.getLogger('modbus_lib')
+        self.is_logger_on = is_logger_on
 
-def f3_create_request_pdu(starting_address, quantity_registers):
-    pdu = bytearray([3])
-    pdu += struct.pack('>H',starting_address)
-    pdu += struct.pack('>H',quantity_registers)
-    return pdu
+    def f3_create_request_pdu(self, starting_address, quantity_registers):
+        pdu = bytearray([3])
+        pdu += struct.pack('>H',starting_address)
+        pdu += struct.pack('>H',quantity_registers)
+        return pdu
 
-def data_from_response_pdu(response_pdu, requested_function, requested_bytes):
-    function = response_pdu[0]
-    if function == requested_function:
-        data = response_pdu[1:]
-        if len(data) == requested_bytes:
-            return data
-        else:
-            raise exceps.ModbusInvalidResponseLengthError
-    elif function == ( requested_function & 0x80):
-        exception_code = response_pdu[1]
-        if exception_code == 1:
-            raise exceps.ModbusExceptionIllegalFunction
-        elif exception_code == 2:
-            raise exceps.ModbusExceptionIllegalDataAddress
-        elif exception_code == 3:
-            raise exceps.ModbusExceptionIllegalDataValue
-        elif exception_code == 4:
-            raise exceps.ModbusExceptionServerDeviceFailure
-        elif exception_code == 5:
-            raise exceps.ModbusExceptionAcknowledge
-        elif exception_code == 6:
-            raise exceps.ModbusExceptionServerDeviceBusy        
-    raise exceps.ModbusUnknownException
+    def data_from_response_pdu(self, response_pdu, requested_function, requested_bytes):
+        function = response_pdu[0]
+        if function == requested_function:
+            data = response_pdu[1:]
+            if len(data) == requested_bytes:
+                return data
+            else:
+                if self.is_logger_on:
+                    self.logger.debug(f'Requested bytes: {requested_bytes} - Received bytes: {len(data)}')
+                    self.logger.error('ModbusInvalidResponseLengthError')
+                raise exceps.ModbusInvalidResponseLengthError
+        elif function == ( requested_function & 0x80):
+            exception_code = response_pdu[1]
+            if exception_code == 1:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionIllegalFunction')
+                raise exceps.ModbusExceptionIllegalFunction
+            elif exception_code == 2:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionIllegalDataAddress')
+                raise exceps.ModbusExceptionIllegalDataAddress
+            elif exception_code == 3:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionIllegalDataValue')
+                raise exceps.ModbusExceptionIllegalDataValue
+            elif exception_code == 4:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionServerDeviceFailure')
+                raise exceps.ModbusExceptionServerDeviceFailure
+            elif exception_code == 5:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionAcknowledge')
+                raise exceps.ModbusExceptionAcknowledge
+            elif exception_code == 6:
+                if self.is_logger_on:
+                    self.logger.error('ModbusExceptionServerDeviceBusy')
+                raise exceps.ModbusExceptionServerDeviceBusy    
+        if self.is_logger_on:
+            self.logger.error('ModbusUnknownException')    
+        raise exceps.ModbusUnknownException
 
 
 if __name__ == "__main__":
--- modbus_lib/modbus_master.py
+++ modbus_lib/modbus_master.py
@@ -1,7 +1,7 @@
 import logging
 
 from modbus_lib.modbus_serial import ModbusSerialLayer
-import modbus_lib.modbus_application as ModApp
+from modbus_lib.modbus_application import ModbusApplicationLayer
 
 
 class ModbusMaster():
@@ -18,9 +18,10 @@ class ModbusMaster():
         self.logger = logging.getLogger('modbus_lib')
         self.logger.setLevel(logger_level)
         self.serial_layer = ModbusSerialLayer(ser, response_timeout_s, interframe_timeout_s, is_logger_on)
+        self.app_layer = ModbusApplicationLayer(is_logger_on)
 
     def read_holding_register(self, slave_address, starting_address, quantity_registers):
-        pdu = ModApp.f3_create_request_pdu(starting_address, quantity_registers)
+        pdu = self.app_layer.f3_create_request_pdu(starting_address, quantity_registers)
         response_pdu = self.serial_layer.send_request_unicast(slave_address, pdu)
-        data = ModApp.data_from_response_pdu(response_pdu, 3, quantity_registers*2)
+        data = self.app_layer.data_from_response_pdu(response_pdu, 3, quantity_registers*2)
         return data
